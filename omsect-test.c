26e9f0a40338f45dba1e7ee2a0b64d0259e5a4ae
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-11-02 00:13:24 -0500

    Provide tests not triggering assertions
    These are the correct set of input permutations.

--- omsect-test.c
+++ omsect-test.c
@@ -9,13 +9,13 @@ FCT_BGN()
     {
         // Table driven test inputs and expected results
         // Test index   =  0 1 2 3 4 5 6 7 8 9 0 1
-        const int a[12] = {1,1,1,1,1,1,3,3,3,4,4,4};
-        const int b[12] = {2,2,3,3,4,4,1,1,4,1,1,3};
-        const int x[12] = {3,4,2,4,2,3,2,4,1,2,3,1};
-        const int y[12] = {4,3,4,2,3,2,4,2,2,3,2,2};
-        const int I[12] = {0,0,1,1,1,1,1,1,0,1,1,0};
-        const int L[12] = {0,0,2,3,2,3,2,3,0,2,3,0};
-        const int H[12] = {0,0,3,2,3,2,3,2,0,3,2,0};
+        const int a[12] = {1,1,1,1,1,1,2,2,2,2,3,3};
+        const int b[12] = {2,2,3,3,4,4,3,3,4,4,4,4};
+        const int x[12] = {3,4,2,4,2,3,1,4,1,3,1,2};
+        const int y[12] = {4,3,4,2,3,2,4,1,3,1,2,1};
+        const int I[12] = {0,0,1,1,1,1,1,1,1,1,0,0};
+        const int L[12] = {0,0,2,3,2,3,2,3,2,3,0,0};
+        const int H[12] = {0,0,3,2,3,2,3,2,3,2,0,0};
 
         // Test each column in the table above
         for (size_t j = 0; j < 12; ++j) {
