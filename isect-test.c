5f04227d2fe75a3df6204e791223713c2323b4d6
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-11-02 21:27:24 -0500

    Change mind on licensing to MPL2
    Having read a touch on the UNLICENSE, I've decided to flip this
    work over to MPL2.  In particular, I want to incorporate portions
    of it into my GPL projects and I'm uncertain of how the UNLICENSE
    would play out in that context.

--- isect-test.c
+++ isect-test.c
@@ -1,4 +1,10 @@
-/* This is free and unencumbered software released into the public domain. */
+/*
+ * Copyright (C) 2013 Rhys Ulerich
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 #include "fct.h"
 #include "isect.h"
